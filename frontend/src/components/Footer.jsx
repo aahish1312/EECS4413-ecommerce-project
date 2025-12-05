887e02230de23399cc154437d70a7681061c7f04
Farhan <farhan.a3hd@gmail.com>
2025-12-05 03:34:29 -0500

    implement navbar and footer components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,15 @@
-function Footer() {
-  return <footer>Footer placeholder</footer>
-}
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">Made by Farhan, Aahish, Abel, Dessert</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
